3bbbfaabb863f2fda9fc1ad1a5bfd065d84f6028
Daniel <danielsolomon94.ds@gmail.com>
2017-05-27 11:57:08 +0300

    Done CKY algorithm, added generated sent from CNF grammar (grammar2.txt) which can be used to test.

--- assignment4/code/cky.py
+++ assignment4/code/cky.py
@@ -1,5 +1,5 @@
 from PCFG import PCFG
-import math
+import collections, math
 
 def load_sents_to_parse(filename):
     sents = []
@@ -10,11 +10,68 @@ def load_sents_to_parse(filename):
                 sents.append(line)
     return sents
 
+def calculate_pcfg_rule_probs(pcfg):
+    rules       = pcfg._rules
+    sums        = pcfg._sums
+    rule_probs  = collections.defaultdict(float)
+
+    for key, derived in rules.items():
+        for derive in derived:
+            rule_probs[(key, tuple(derive[0]))] = derive[1] / sums[key]
+    return rule_probs
+
+def gentree_by_bp(sent, bp, i, j, r):
+    rule, s     = bp[(i, j, r)]
+    if s is None:
+        return '({} {})'.format(rule[0], rule[1][0])
+    left        = rule[1][0]
+    right       = rule[1][1]
+    left_tree   = gentree_by_bp(sent, bp, i, s, left)
+    right_tree  = gentree_by_bp(sent, bp, s + 1, j, right)
+    return '({} ({} {}))'.format(rule[0], left_tree, right_tree)
+    
 def cky(pcfg, sent):
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
-    return "FAILED TO PARSE!"
+    sent = sent.split()
+    
+    pi          = collections.defaultdict(float)
+    rule_probs  = calculate_pcfg_rule_probs(pcfg)
+    bp          = {}
+        
+    # base
+    for i in range(len(sent)):
+        for r in pcfg._rules.keys():
+            pi[(i, i, r)] = rule_probs[(r, (sent[i],))]
+            bp[(i, i, r)] = ((r, (sent[i],)), None)
+            
+    # recursion
+    for l in range(1, len(sent)):
+        for i in range(len(sent) - 1):
+            j = i + l
+            for r in pcfg._rules.keys():
+                max_prob  = 0.0
+                best_rule = None
+                best_s    = -1
+                for s in range(i, j):
+                    for rule in pcfg._rules[r]:
+                        rule    = tuple(rule[0])
+                        if pcfg.is_preterminal(rule):
+                            continue
+                        i_s     = pi[(i, s, rule[0])]
+                        s_j     = pi[(s + 1, j, rule[1])]
+                        q_prob  = rule_probs[(r, rule)]  
+
+                        prob    = i_s * s_j * q_prob
+                        if prob > max_prob:
+                            max_prob  = prob
+                            best_rule = (r, rule)
+                            best_s    = s
+                pi[(i, j, r)] = max_prob
+                bp[(i, j, r)] = best_rule, best_s
+    
+    if pi[(0, len(sent) - 1, 'S')] > 0:
+        return gentree_by_bp(sent, bp, 0, len(sent) - 1, 'S')
+    else:
+        return "FAILED TO PARSE!"
 
 if __name__ == '__main__':
     import sys
